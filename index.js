ac14789664450aef0cdc92fb7aba7ce471572b24
kaelzhang <i@kael.me>
2014-05-18 16:09:34 +0800

    finish main functionalities and test case

---
+++ index.js
@@ -0,0 +1,101 @@
+'use strict';
+
+module.exports = gu;
+gu.GithubURL = GithubURL;
+
+var su = require('ssh-url');
+var node_url = require('url');
+
+var REGEX_HAS_PROTOCOL = /^[a-z]+:\/\//i;
+var REGEX_GIT_EXT = /\.git$/i
+
+
+function gu (str) {
+  return new gu.GithubURL(str);
+}
+
+function GithubURL (str) {
+  gu._parse(str, this);
+}
+
+var proto = GithubURL.prototype;
+
+var GETTERS = {
+  https_href: function () {
+    return 'https://' + gu._formatURL(this);
+      
+  },
+  https_clone_url: function () {
+    return 'https://' + gu._formatURL(this) + '.git';
+  },
+  ssh_clone_url: function () {
+    return gu._formatURL(this, ':', true) + '.git';
+  },
+  git_clone_url: function () {
+    return 'git://' + gu._formatURL(this) + '.git';
+  }
+};
+
+gu.GETTERS = GETTERS;
+
+
+Object.keys(GETTERS).forEach(function (key) {
+  var value_key = '_' + key;
+
+  // Could be listed out by `console.log` but not by for-in
+  // Object.defineProperty(proto, value_key, {
+  //   writalbe: true
+  // });
+
+  Object.defineProperty(proto, key, {
+    enumerable: true,
+    get: function () {
+      return this[value_key] || (this[value_key] = GETTERS[key].call(this));
+    }
+  });
+});
+
+
+gu._parse = function (str, host) {
+  var parsed;
+  if (REGEX_HAS_PROTOCOL.test(str)) {
+    parsed = node_url.parse(str);
+    host.host = parsed.host;
+    host.ssh_user = parsed.auth;
+
+  } else {
+    parsed = su.parse(str);
+    host.host = parsed.hostname;
+    host.ssh_user = parsed.user;
+  }
+
+  var info = gu._infoFromPathname(parsed.pathname);
+  host.repo = info.repo;
+  host.user = info.user;
+};
+
+
+gu._infoFromPathname = function (pathname) {
+  var split = pathname.split('/').filter(Boolean);
+  return {
+    user: split[0],
+    repo: (split[1] || '').replace(REGEX_GIT_EXT, '')
+  };
+};
+
+gu._formatURL = function (parsed, splitter, is_ssh) {
+  var ssh_user = parsed.ssh_user || 'git';
+  
+  if (!is_ssh && ssh_user === 'git') {
+    ssh_user = null;
+  }
+
+  return (
+      ssh_user 
+        ? ssh_user + '@'
+        : ''
+    ) +
+    parsed.host +
+    (splitter || '/') + parsed.user + 
+    '/' + parsed.repo;
+}
--- test/githuburl.js
+++ test/githuburl.js
@@ -1,4 +1,67 @@
 'use strict';
 
 var expect = require('chai').expect;
-var githuburl = require('../');
+var gu = require('../');
+
+var cases = [
+{
+  user: 'kaelzhang',
+  repo: 'node-githuburl',
+  ssh_user: 'git',
+  host: 'github.com',
+  
+  // Actually the properties below are all getters,
+  // which you need not to concern about the performance.
+  https_href: 'https://github.com/kaelzhang/node-githuburl',
+  https_clone_url: 'https://github.com/kaelzhang/node-githuburl.git',
+  ssh_clone_url: 'git@github.com:kaelzhang/node-githuburl.git',
+  git_clone_url: 'git://github.com/kaelzhang/node-githuburl.git' 
+},
+{
+  user: 'kaelzhang',
+  repo: 'node-githuburl',
+  ssh_user: 'vip',
+  host: 'vip.github.com',
+  _check_all_ssh: true,
+
+  // Actually the properties below are all getters,
+  // which you need not to concern about the performance.
+  https_href: 'https://vip@vip.github.com/kaelzhang/node-githuburl',
+  https_clone_url: 'https://vip@vip.github.com/kaelzhang/node-githuburl.git',
+  ssh_clone_url: 'vip@vip.github.com:kaelzhang/node-githuburl.git',
+  git_clone_url: 'git://vip@vip.github.com/kaelzhang/node-githuburl.git' 
+},
+];
+
+
+var GETTERS = Object.keys(gu.GETTERS);
+var keys = [
+  'user',
+  'repo',
+  'ssh_user',
+  'host'
+];
+
+describe("githuburl", function(){
+  cases.forEach(function (c) {
+    GETTERS.forEach(function (key) {
+      var url = c[key];
+      it(url, function(){
+        var parsed = gu(url);
+        var check_all = c._check_all_ssh;
+
+        keys.forEach(function (k) {
+          if (!check_all && k === 'ssh_user' && key !== 'ssh_clone_url') {
+            return;
+          }
+
+          expect(parsed[k]).to.equal(c[k]);
+        });
+
+        GETTERS.forEach(function (k) {
+          expect(parsed[k]).to.equal(c[k]);
+        });
+      });
+    })
+  });
+});
